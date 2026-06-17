b276ac557c91e1311fb16b2a70638341f6cef0ea
Dorival Freitas <dorivalc.freitas@gmail.com>
2026-06-17 20:30:06 -0300

    Primeiro commit do site com imagens

---
+++ enviar.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensagem = htmlspecialchars($_POST['mensagem']);
+
+    $to = "contato@seudominio.com"; // coloque seu e-mail real aqui
+    $subject = "Novo contato do site";
+    $body = "Nome: $nome\nEmail: $email\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar a mensagem. Tente novamente.";
+    }
+}
+?>
